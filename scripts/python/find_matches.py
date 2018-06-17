27836a1d334313432332883ffdf337eef866b3d0
Mark Needham <mark.needham@neotechnology.com>
2018-06-17 18:33:54 +0100

    formatting

--- scripts/python/find_matches.py
+++ scripts/python/find_matches.py
@@ -3,26 +3,26 @@ import csv
 from bs4 import BeautifulSoup
 
 world_cups = {
-	2014:  "https://www.fifa.com/worldcup/archive/brazil2014/matches/",
-	2010 : "https://www.fifa.com/worldcup/archive/southafrica2010/matches/",
-	2006 : "https://www.fifa.com/worldcup/archive/germany2006/matches",
-	2002 : "https://www.fifa.com/worldcup/archive/koreajapan2002/matches",
-	1998 : "https://www.fifa.com/worldcup/archive/france1998/matches",
-	1994 : "https://www.fifa.com/worldcup/archive/usa1994/matches",
-	1990 : "https://www.fifa.com/worldcup/archive/italy1990/matches",
-	1986 : "https://www.fifa.com/worldcup/archive/mexico1986/matches",
-	1982 : "https://www.fifa.com/worldcup/archive/spain1982/matches",
-	1978 : "https://www.fifa.com/worldcup/archive/argentina1978/matches",
-	1974 : "https://www.fifa.com/worldcup/archive/germany1974/matches",
-	1970 : "https://www.fifa.com/worldcup/archive/mexico1970/matches",
-	1966 : "https://www.fifa.com/worldcup/archive/england1966/matches",
-	1962 : "https://www.fifa.com/worldcup/archive/chile1962/matches",
-	1958 : "https://www.fifa.com/worldcup/archive/sweden1958/matches",
-	1954 : "https://www.fifa.com/worldcup/archive/switzerland1954/matches",
-	1950 : "https://www.fifa.com/worldcup/archive/brazil1950/matches",
-	1938 : "https://www.fifa.com/worldcup/archive/france1938/matches",
-	1934 : "https://www.fifa.com/worldcup/archive/italy1934/matches",
-	1930 : "https://www.fifa.com/worldcup/archive/uruguay1930/matches"
+	2014: "https://www.fifa.com/worldcup/archive/brazil2014/matches/",
+	2010: "https://www.fifa.com/worldcup/archive/southafrica2010/matches/",
+	2006: "https://www.fifa.com/worldcup/archive/germany2006/matches",
+	2002: "https://www.fifa.com/worldcup/archive/koreajapan2002/matches",
+	1998: "https://www.fifa.com/worldcup/archive/france1998/matches",
+	1994: "https://www.fifa.com/worldcup/archive/usa1994/matches",
+	1990: "https://www.fifa.com/worldcup/archive/italy1990/matches",
+	1986: "https://www.fifa.com/worldcup/archive/mexico1986/matches",
+	1982: "https://www.fifa.com/worldcup/archive/spain1982/matches",
+	1978: "https://www.fifa.com/worldcup/archive/argentina1978/matches",
+	1974: "https://www.fifa.com/worldcup/archive/germany1974/matches",
+	1970: "https://www.fifa.com/worldcup/archive/mexico1970/matches",
+	1966: "https://www.fifa.com/worldcup/archive/england1966/matches",
+	1962: "https://www.fifa.com/worldcup/archive/chile1962/matches",
+	1958: "https://www.fifa.com/worldcup/archive/sweden1958/matches",
+	1954: "https://www.fifa.com/worldcup/archive/switzerland1954/matches",
+	1950: "https://www.fifa.com/worldcup/archive/brazil1950/matches",
+	1938: "https://www.fifa.com/worldcup/archive/france1938/matches",
+	1934: "https://www.fifa.com/worldcup/archive/italy1934/matches",
+	1930: "https://www.fifa.com/worldcup/archive/uruguay1930/matches"
 }
 
 with open("data/2018/matches.csv", "w") as matches_file:
@@ -56,5 +56,6 @@ with open("data/2018/matches.csv", "w") as matches_file:
 				link, result
 			]
 
-			values = [value.strip() if type(value) is str else value for value in values]
+			values = [value.strip() if type(value) is str else value
+					  for value in values]
 			writer.writerow(values)
