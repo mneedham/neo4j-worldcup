a81426045dbc77b7016b83b5b2efd85a5d3b0193
Mark Needham <mark.needham@neotechnology.com>
2018-06-18 22:20:29 +0100

    add 2018 matches

--- scripts/python/matches_to_csv.py
+++ scripts/python/matches_to_csv.py
@@ -3,7 +3,7 @@ import glob
 from bs4 import BeautifulSoup
 import re
 
-with open("data/2018/import/matches.csv", "w") as matches_file:
+with open("data/2018/import/matches2.csv", "w") as matches_file:
     writer = csv.writer(matches_file, delimiter=",")
 
     writer.writerow([
@@ -11,8 +11,8 @@ with open("data/2018/import/matches.csv", "w") as matches_file:
         "home_id", "home", "home_code",
         "away_id", "away", "away_code",
         "h_score", "a_score",
-        "date", "stadium",  "round",
-        "world_cup",  "year", "host",
+        "date", "stadium", "round",
+        "world_cup", "year", "host",
         "reasonWin"
     ])
 
@@ -22,41 +22,77 @@ with open("data/2018/import/matches.csv", "w") as matches_file:
         with open(match_file, "r") as file:
             soup = BeautifulSoup(file.read(), "html.parser")
 
-            header = soup.select("div h1 a")[0].text
-            groups = re.match("(\d{4}) FIFA World Cup (.*?)™", header)
+            header_element = soup.select("div h1 a")
+
+            if len(header_element) == 0:
+                header_element = soup.select("div a h1")
+
+            header = header_element[0].text
+
+            groups = re.match("(\d{4}) FIFA World Cup ([a-zA-Z ]*)", header)
             world_cup = groups[0]
-            year = groups[1]
+            year = int(groups[1])
             host = groups[2]
 
             if year == "1974":
                 host = "Germany FR"
 
             home_element = soup.select("div.home")
-
             home_id = home_element[0]["data-team-id"]
-            home = home_element[0].select("span.t-nText")[0].text
-            home_code = home_element[0].select("span.t-nTri")[0].text
+
+            team_selector = "span.fi-t__nText" if year == 2018 else "span.t-nText"
+            code_selector = "span.fi-t__nTri" if year == 2018 else "span.t-nTri"
+
+            home = home_element[0].select(team_selector)[0].text
+            home_code = home_element[0].select(code_selector)[0].text
 
             away_element = soup.select("div.away")
 
             away_id = away_element[0]["data-team-id"]
-            away = away_element[0].select("span.t-nText")[0].text
-            away_code = away_element[0].select("span.t-nTri")[0].text
+            away = away_element[0].select(team_selector)[0].text
+            away_code = away_element[0].select(code_selector)[0].text
 
-            round = soup.select("div.mh-i-round")[0].text
+            round_selector = "span.fi__info__group" if year == 2018 else "div.mh-i-round"
+
+            round = soup.select(round_selector)[0].text
 
             if round == "Play-off for third place":
                 round = "Match for third place"
 
-            location_date_element = soup.select("div.mh-i-location-date")[0]
-            stadium = location_date_element.select("span.mh-i-stadium")[0].text
-            venue = location_date_element.select("span.mh-i-venue")[0].text
+            location_date_element_selector = "div.fi__info__location" \
+                if year == 2018 else "div.mh-i-location-date"
+            location_date_element = soup.select(location_date_element_selector)[0]
 
-            date = soup.select("div.mh-i-datetime")[0].text
+            stadium_selector = "span.fi__info__stadium" if year == 2018 else "span.mh-i-stadium"
 
-            h_score, a_score = soup.select("span.s-scoreText")[0].text.split("-")
+            stadium = location_date_element.select(stadium_selector)[0].text
 
-            reason_win = soup.select("div.mu-reasonwin span.text-reasonwin")[0].text
+            venue_selector = "span.fi__info__venue" if year == 2018 else "span.mh-i-venue"
+            venue = location_date_element.select(venue_selector)[0].text
+
+            date_selector = "div.fi-mu__info__datetime" if year == 2018 else "div.mh-i-datetime"
+            date = soup.select(date_selector)[0].text.replace('\n', '')
+
+            score_selector = "span.fi-s__scoreText" if year == 2018 else "span.s-scoreText"
+            score = soup.select(score_selector)[0].text.split("-")
+
+            h_score = a_score = ""
+            if len(score) == 2:
+                h_score, a_score = score
+
+            if year == 2018:
+                reasons_selector = "div.fi-mu__details div.fi-mu__reasonwin-wrap div.fi-mu__reasonwin"
+                reason_win_elements = [
+                    element
+                    for element in soup.select(reasons_selector)
+                    if not "hidden" in element.find_parent()["class"]
+                ]
+
+                if len(reason_win_elements) > 0:
+                    reason_win = reason_win_elements[0].text
+            else:
+                reason_win_selector = "div.mu-reasonwin span.text-reasonwin"
+                reason_win = soup.select(reason_win_selector)[0].text
 
             values = [
                 match_id,
@@ -66,7 +102,6 @@ with open("data/2018/import/matches.csv", "w") as matches_file:
                 date, stadium, round,
                 world_cup, year, host,
                 reason_win
-
             ]
             values = [value.strip() if type(value) is str else value for value in values]
             writer.writerow(values)
